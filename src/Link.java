1f950d21df4b8804779ba2bec3392212f3dad8da
Andy Rohde <andrewprohde@gmail.com>
2020-04-08 22:10:55 -0500

    added enemy and link classes

--- src/Link.java
+++ src/Link.java
@@ -1,30 +1,42 @@
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Link {
-
     int x = 0;
+    int xa = 0;
     int y = 0;
-    int xa = 1;
-    int ya = 1;
+    int ya = 0;
     private Main game;
 
     public Link(Main game) {
         this.game = game;
     }
-    void move() {
-        if (x + xa < 0)
-            xa = 1;
-        if (x + xa > game.getWidth() - 30)
-            xa = -1;
-        if (y + ya < 0)
-            ya = 1;
-        if (y + ya > game.getHeight() - 30)
-            ya = -1;
 
-        x = x + xa;
-        y = y + ya;
+    public void move() {
+        if (x + xa > 0 && x + xa < game.getWidth() - 60)
+            x = x + xa;
+        if (y + ya > 0 && y + ya < game.getHeight() - 60)
+            y = y + ya;
     }
-    public  void paint(Graphics2D g) {
-        g.fillOval(x, y, 30, 30);
+
+    public void paint(Graphics2D g) {
+        g.fillRect(x, y, 24, 32);
     }
+
+    public void keyReleased(KeyEvent e) {
+        xa = 0;
+        ya = 0;
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_LEFT)
+            xa = -1;
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+            xa = 1;
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+            ya = -1;
+        if (e.getKeyCode() == KeyEvent.VK_DOWN)
+            ya = 1;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,35 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 @SuppressWarnings("serial")
 public class Main extends  JPanel {
 
+    Enemy enemy = new Enemy(this);
     Link link = new Link(this);
 
+    public Main() {
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                link.keyPressed(e);
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                link.keyReleased(e);
+            }
+        });
+        setFocusable(true);
+    }
+
     private void move() {
+        enemy.move();
         link.move();
     }
     @Override
@@ -15,6 +37,7 @@ public class Main extends  JPanel {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        enemy.paint(g2d);
         link.paint(g2d);
     }
 
